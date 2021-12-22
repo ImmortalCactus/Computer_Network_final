2039ca434d115b706aa6e0e11dc0d62f5bf53c6c
b08902023 <b08902023@linux8.csie.ntu.edu.tw>
2021-12-22 20:07:55 +0800

    rough http request parsing

--- transfer.cpp
+++ transfer.cpp
@@ -8,9 +8,9 @@
 #include <iostream>
 #include <algorithm>
 #define BUF_SIZE 1024
+using namespace std;
 
-
-bool exists_test(const std::string& name){
+bool exists_test(const string& name){
     struct stat buffer;   
     return (stat (name.c_str(), &buffer) == 0); 
 }
@@ -21,20 +21,20 @@ void recv_file(int sockfd, const char* filename)
     fp = fopen(filename, "wb");
     int size;
     read(sockfd, &size, sizeof(int));
-    //std::cout<<"should receive "<<size<<std::endl;
+    //cout<<"should receive "<<size<<endl;
 
     char buffer[BUF_SIZE];
     int bytes_received = 0;
     
     while(bytes_received < size){
         size_t cur_bytes_received;
-        cur_bytes_received = read(sockfd, buffer, std::min(sizeof(buffer), (long unsigned int)(size-bytes_received)));
-        //std::cout<<"received "<<cur_bytes_received<<std::endl;
+        cur_bytes_received = read(sockfd, buffer, min(sizeof(buffer), (long unsigned int)(size-bytes_received)));
+        //cout<<"received "<<cur_bytes_received<<endl;
         bytes_received += cur_bytes_received;
         fwrite(buffer, 1, cur_bytes_received, fp);
     }
     fclose(fp);
-    //std::cout<<"FINISHED RECV_FILE!\n";
+    //cout<<"FINISHED RECV_FILE!\n";
     
 }
 
@@ -48,7 +48,7 @@ void send_file(int sockfd,  const char* filename)
     size = (int)st.st_size;
 
     write(sockfd, &size, sizeof(size));
-    //std::cout<<"should send "<<size<<std::endl;
+    //cout<<"should send "<<size<<endl;
 
     char buffer[BUF_SIZE];
     int bytes_sent = 0, bytes_read;
@@ -59,7 +59,7 @@ void send_file(int sockfd,  const char* filename)
         size_t cur_bytes_sent = 0;
         while(cur_bytes_sent<bytes_read){
             int n = write(sockfd, buffer+cur_bytes_sent, bytes_read-cur_bytes_sent);
-            //std::cout<<"sent "<<n<<std::endl;
+            //cout<<"sent "<<n<<endl;
             if(n==-1){
                 fclose(fp);
                 return;
@@ -69,27 +69,27 @@ void send_file(int sockfd,  const char* filename)
         bytes_sent += cur_bytes_sent;
     }
     fclose(fp);
-    //std::cout<<"FINISHED SEND_FILE!\n";
+    //cout<<"FINISHED SEND_FILE!\n";
 }
 
-std::string recv_str(int sockfd){
+string recv_str(int sockfd){
     int size;
     read(sockfd, &size, sizeof(int));
 
     char buffer[BUF_SIZE];
     int bytes_received = 0;
 
-    std::string str = "";
+    string str = "";
     while(bytes_received < size){
         int cur_bytes_received = read(sockfd, buffer, size-bytes_received);
         bytes_received += cur_bytes_received;
         buffer[cur_bytes_received] = '\0';
-        str = str + std::string(buffer);
+        str = str + string(buffer);
     }
     return str;
 
 }
-void send_str(int sockfd, const std::string& str){
+void send_str(int sockfd, const string& str){
     int size = str.length();
     write(sockfd, &size, sizeof(size));
     int bytes_sent = 0;
@@ -99,3 +99,43 @@ void send_str(int sockfd, const std::string& str){
     }
 }
 
+void get_http_request(int sockfd){
+    char buffer[BUF_SIZE+1];
+    int state = 0;
+    int content_length = 0;
+    vector<string> request;
+    string line;
+    while(1){
+        read(sockfd, buffer, 1);
+        if(state == 0 && buffer[0]=='\r')state = 1;
+        else if(state == 1 && buffer[0]=='\n')state = 2;
+        else if(state == 2){
+            if(buffer[0]!='\r'){
+                line += buffer[0];
+            }else{
+                if(line.substr(0, 16) == "content-length: "){
+                    stringstream i_ss(line.substr(16));
+                    i_ss >> content_length;
+                }
+                if(line.length() == 0){
+                    state = 3;
+                }else{
+                    state = 1;
+                    request.push_back(line);
+                }
+            }
+        }
+        else if(state == 3 && buffer[0]=='\n'){
+            break;
+        }
+    }
+    
+    string content = "";
+    int bytes_received = 0;
+    while(bytes_received < content_length){
+        int cur_bytes_received = read(sockfd, buffer, size-bytes_received);
+        bytes_received += cur_bytes_received;
+        buffer[cur_bytes_received] = '\0';
+        str = str + string(buffer);
+    }
+}
